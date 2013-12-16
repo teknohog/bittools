d7e3bd9772efb9d34247ba093fc6f0fe7fc53a2f
Risto A. Paju <teknohog@iki.fi>
2013-12-16 21:13:27 +0200

    Primecoin mining calc refinement, as per mikaelh

--- scripts/coinfo.py
+++ scripts/coinfo.py
@@ -425,7 +425,7 @@ if hashrate > 0:
 
         # hashrate = chainsperday
         frac = diff - int(diff)
-        time = 86400 / (hashrate * (1 - frac))
+        time = 86400 / (hashrate * (1 - frac + 0.035))
     else:
         time = diff * 2**32 / hashrate
 
